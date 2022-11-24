ac5f6e588b5aece546688d793a0440637d161900
Issawat <actiondeveloper1998@gmail.com>
2022-11-24 23:00:55 +0700

    chore: remove header

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,7 +5,6 @@ import type { ReactNode } from "react"
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
-      <Header />
       <main>{children}</main>
       <Footer />
     </>
